b9a09338601e36257fcb13195d4afffdc3d6d3a3
Jason Rhoads <jasrhoads@gmail.com>
2023-10-03 11:12:12 -0700

    Display cyberware options

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-console.log("hello from index.js");
-console.log(cyberware);
 
-document.querySelector('#test').innerHTML = 'test';
-document.querySelector('#test2').innerHTML = 'test';
+//Populate cyberware options into containers
+
+for (const type in cyberware) {
+    document.querySelector(`#${type}-container > .container-header > h2`)
+            .innerHTML = type.toUpperCase();
+    // console.log(cyberware[type]);
+    for (const option of cyberware[type].options) {
+
+        document.querySelector(`#${type}-options > .options-container`)
+            .innerHTML += `
+                <div class="cyberware-option">
+                <input type="checkbox" name="${option.name}" id="${option.name}">
+                <label class="option option-name" for="${option.name}">${option.name}</label>
+                <label class="option option-install" for="${option.name}">${option.install}</label>
+                <label class="option option-description" for="${option.name}">${option.description}</label>
+                <label class="option option-cost" for="${option.name}">${option.cost}</label>
+                <label class="option option-HL" for="${option.name}">${option.humanityLoss}</label>
+                </div>
+            `
+        // console.log(option);
+    }
+}
+
+
+
+
